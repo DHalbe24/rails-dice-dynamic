b377f8abd18d8155d6ea22414ad8d4d57bdf9ecc
DHalbe24 <137205368+DHalbe24@users.noreply.github.com>
2023-07-26 20:42:37 +0000

    created routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
+  get("/dice/2/6", { :controller => "dice", :action => "2d6" })
+  
+  get("/dice/2/10", { :controller => "dice", :action => "2d10" })
 
+  get("/dice/1/20", { :controller => "dice", :action => "1d20" })
+
+  get("dice/5/4", { :controller => "dice", :action => "5d4" })
+
+  get("/dice/number/sides", { :controller => "dice", :action => "dynamic" })
+
+  get("/", { :controller => "home", :action => "home" })
 end
